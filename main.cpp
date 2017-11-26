00a6bb72762c7cb864422c43005e86d4561c6937
Katie Grosch <katie.grosch@Katies-MacBook.local>
2017-11-26 06:30:46 -0900

    uncommented switch statement

--- main.cpp
+++ main.cpp
@@ -10,31 +10,39 @@ typedef CGAL::Polygon_2<Kernel> Polygon_2;
 
 int main()
 {
-  // Point_2 p(1,1), q(10,10);
-  // cout << "point p = " << p << endl;
-  // cout << "point q = " << q.x() << " " << q.y() << endl;
-  // cout << "sqdist(p,q) = " 
-  //           << CGAL::squared_distance(p,q) << endl;
+  while (false) {
+    int x1, y1;
+    cin >> x1 >> y1;
+    Point_2 pt(x1, y1);
+  }
+
+  Point_2 p(1,1), q(10,10), m(5,9);
+  cout << "point p = " << p << endl;
+  cout << "point q = " << q.x() << " " << q.y() << endl;
+  cout << "point m = " << m.x() << " " << m.y() << endl;
+  cout << "sqdist(p,q) = " 
+            << CGAL::squared_distance(p,q) << endl;
   
-  // Segment_2 s(p,q);
-  // Point_2 m(5, 9);
-  
-  // cout << "m = " << m << endl;
-  // cout << "sqdist(Segment_2(p,q), m) = "
-  //           << CGAL::squared_distance(s,m) << endl;
-  // cout << "p, q, and m ";
-  // switch (CGAL::orientation(p,q,m)){
-  // case CGAL::COLLINEAR: 
-  //   cout << "are collinear\n";
-  //   break;
-  // case CGAL::LEFT_TURN:
-  //   cout << "make a left turn\n";
-  //   break;
-  // case CGAL::RIGHT_TURN: 
-  //   cout << "make a right turn\n";
-  //   break;
-  // }
-  // cout << " midpoint(p,q) = " << CGAL::midpoint(p,q) << endl;
+  Segment_2 s(p,q);
+
+  cout << "sqdist(Segment_2(p,q), m) = "
+            << CGAL::squared_distance(s,m) << endl;
+
+  cout << "p, q, and m ";
+
+  switch (CGAL::orientation(m, q, p)) {
+    case CGAL::COLLINEAR:
+      cout << "are collinear" << endl;
+      break;
+    case CGAL::LEFT_TURN:
+      cout << "make a left turn" << endl;
+      break;
+    case CGAL::RIGHT_TURN:
+      cout << "make a right turn" << endl;
+      break;
+  }
+
+  cout << " midpoint(p,q) = " << CGAL::midpoint(p,q) << endl;
 
   Point_2 points[3];
 
